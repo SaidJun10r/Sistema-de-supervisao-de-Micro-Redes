48a4de20aa224246ffddeab6426992640071334c
SaidJun10r <said_junior2015@outlook.com>
2024-06-11 14:09:54 -0300

    SIM.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from leitor import leitorcsv
 import modelos
 from grafico import grafico
-import customtkinter
+import tkinter, customtkinter
 
 dados = 'Dados.csv'
 
@@ -15,12 +15,31 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("400x240")
+app.title("TCC II")
+
+app.grid_columnconfigure((0), weight=1)
+app.grid_columnconfigure((1), weight=4)
+app.grid_rowconfigure((0), weight=4)
+app.grid_rowconfigure((1), weight=1)
+
 
 def button_function():
     grafico(horas, carga, gerSolar, cargaVE==None, bateria, rede)
 
-# Use CTkButton instead of tkinter Button
+# Botão
 button = customtkinter.CTkButton(master=app, text="Estudo de Caso 2", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+button.grid(row=0, column=1, padx=20, pady=10, sticky="news")
+
+# Botão
+button2 = customtkinter.CTkButton(master=app, text="Estudo de Caso 2", command=button_function)
+button2.grid(row=0, column=0, padx=20, pady=10, sticky="news")
+
+# Botão
+button3 = customtkinter.CTkButton(master=app, text="Estudo de Caso 2", command=button_function)
+button3.grid(row=1, column=1, padx=20, pady=10, sticky="news")
+
+# Botão
+button4 = customtkinter.CTkButton(master=app, text="Estudo de Caso 2", command=button_function)
+button4.grid(row=1, column=0, padx=20, pady=10, sticky="news")
 
 app.mainloop()
